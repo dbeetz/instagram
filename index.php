2ace243ef143b780474e0a3c62c4b5f3bb4015cc
Devon Beets <dbeetzz@gmail.com>
2016-07-18 08:19:36 -0600

    Added all of the heading tags

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
-	<html lang = "en">
+	<html lang="en">
 	<head>
 		<meta charset="utf-8" />
 		<title>Instagram Data Design</title>
 	</head>
 	<body>
-		<h1><strong>Instagram</strong></h1>
-		<h2>Persona</h2>
-		<h3>Madsion Green</h3>
+		<h1><strong>Instagram Data Design</strong></h1>
+		<h2><strong>Persona</strong></h2>
+		<h3>Madison Green</h3>
 		<h3>Age 23</h3>
 		<h3>College Student</h3>
 		<h3>Technology</h3>
