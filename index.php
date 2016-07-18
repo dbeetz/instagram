dcc826db03be22677a4cafb09cf805c78398899d
Devon Beets <dbeetzz@gmail.com>
2016-07-18 08:23:48 -0600

    Added paragraph text

--- index.php
+++ index.php
@@ -11,8 +11,12 @@
 		<h3>Age 23</h3>
 		<h3>College Student</h3>
 		<h3>Technology</h3>
+		<p>iPhone 6S with high usage cap 4G data, small PC laptop for at home and on campus use.</p>
 		<h3>Attitudes and Behaviors</h3>
+		<p>Friendly, outgoing, confident, adventurous, constantly connected to social media through FaceBook, SnapChat, Instagram, etc. Loves going out and to travel, and experience new things with friends and family.</p>
 		<h3>Frustrations and Needs</h3>
+		<p>Not necessarily frustrated by her user experience with Instagram, she just needs to be able to share with her numerous friends, family and acquaintances photographs of her experiences and update them on her goings-on. She loves to be able to do this in real time, mobile first (usually never on the computer) and upload photos minutes after they are taken.</p>
 		<h3>Goals</h3>
+		<p>Along with taking photos of her with her friends and of her new experiences, Madison also has the goals of connecting with new acquaintances and friends, and viewing their photographs and shared experiences. As well as with her own photos, she likes to do this in real time and stay up to date with anything new happening with her network of social mediaites. This includes being able to talk with her friends and family by instant messages, comments, and show support for their activities by using the like functionality of the site.</p>
 	</body>
 </html>
