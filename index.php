2da75875940027a65df256611080d6f05a6b32e1
Devon Beets <dbeetzz@gmail.com>
2016-07-18 08:05:47 -0600

    Added all of the heading tags

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+	<html lang = "en">
+	<head>
+		<meta charset="utf-8" />
+		<title>Instagram Data Design</title>
+	</head>
+	<body>
+		<h1><strong>Instagram</strong></h1>
+		<h2>Persona</h2>
+		<h3>Madsion Green</h3>
+		<h3>Age 23</h3>
+		<h3>College Student</h3>
+		<h3>Technology</h3>
+		<h3>Attitudes and Behaviors</h3>
+		<h3>Frustrations and Needs</h3>
+		<h3>Goals</h3>
+	</body>
+</html>
