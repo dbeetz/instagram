59da87ab32f80a2e3aa5aa90673ceb3c364180d4
Devon Beets <dbeetzz@gmail.com>
2016-07-24 20:27:20 -0600

    Created accessor and mutator php functions layout for user id

---
+++ php/classes/user.php
@@ -0,0 +1,33 @@
+<?php
+namespace Edu\Cnm\dbeets\InstagramDataDesign;
+
+/**
+* accessor method for user id
+*
+* @return int|null value of user id
+**/
+public function getUserId() {
+	return($this->userId);
+}
+
+/**
+ * mutator method for user id
+ *
+ * @param int|null $newUserId new value of user id
+ * @throws \RangeException if $newUserId is not positive
+ * @throws \TypeError if $newTweetId is not an integer
+ **/
+public function setUserId(int $newUserId = null) {
+	// if the user id is null, this is a new user id
+	if(newUserId === null) {
+		$this-> userId = null;
+		return;
+	}
+	// verify that the user id is positive
+	if(newUserId <= 0) {
+		throw(new \RangeException("user id is not positive"));
+	}
+	// assign the new user id
+	$this->userId = $newUserId;
+}
+?>
